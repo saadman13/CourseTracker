45636d2ee79c73b61ce0639d6f082ef910013ad5
Saadman Islam Khan <saadmani@ualberta.ca>
2022-05-25 12:22:29 -0600

    setup postgres

--- src/backend/courseTracker/settings.py
+++ src/backend/courseTracker/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'courseTracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'courseTracker',
+        'USER': 'postgres',
+        'PASSWORD': 'helpinghands',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
