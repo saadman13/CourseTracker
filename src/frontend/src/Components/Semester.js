24b250073062ba70f3e208fa89bae3573cac5bc2
Saadman Islam Khan <saadmani@ualberta.ca>
2022-06-19 16:22:53 -0600

    convert to class-based components

--- src/frontend/src/Components/Semester.js
+++ src/frontend/src/Components/Semester.js
@@ -1,44 +1,51 @@
-import React, {useState} from 'react';
+import React from 'react';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import './Semester.css';
 
-const Semester = props => {
-    const [newCourse, setNewCourse] = useState('');
-    const [courses, setCourses] = useState([]);
-
-    const onChangeHandler = (event) => {
-        setNewCourse(event.target.value);
+class Semester extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            newCourse: '',
+            courses: []
+        }
     }
 
-    const addCourseHandler = (event) => {
-        setCourses( prev => [...prev, newCourse])
+    onChangeHandler = (event) => {
+        this.setState({newCourse: event.target.value});
     }
 
-    return (
-        <div>
-            <span className='course-input'>
-                <h2>{props.semesterName}</h2>
-                <Box
-                    component="form"
-                    sx={{
-                        '& > :not(style)': { ml: 20, mt: 6, mr: 0, width: '10ch' },
-                    }}
-                    noValidate
-                    autoComplete="off"
-                >
-                    <TextField onChange={onChangeHandler} id="standard-basic" label="Course Name" variant="standard" />
-                </Box>
-                <button onClick={addCourseHandler} className='btn'>Add Course</button>
-            </span>
-            <ul id='courses'>
-                {courses.map(course => <li><a href={`https://www.google.com/search?q=${course}`}> {course}</a></li>)}
-            </ul>
-            <hr></hr>
-        </div>
-    );
+    addCourseHandler = (event) => {
+        this.setState(state => ({
+            courses: [...state.courses, state.newCourse]
+        }));
+    }
+
+    render(){
+        return (
+            <div>
+                <span className='course-input'>
+                    <h2>{this.props.semesterName}</h2>
+                    <Box
+                        component="form"
+                        sx={{
+                            '& > :not(style)': { ml: 20, mt: 6, mr: 0, width: '10ch' },
+                        }}
+                        noValidate
+                        autoComplete="off"
+                    >
+                        <TextField onChange={this.onChangeHandler} id="standard-basic" label="Course Name" variant="standard" />
+                    </Box>
+                    <button onClick={this.addCourseHandler} className='btn'>Add Course</button>
+                </span>
+                <ul id='courses'>
+                    {this.state.courses.map(course => <li><a href={`https://www.google.com/search?q=${course}`}> {course}</a></li>)}
+                </ul>
+                <hr></hr>
+            </div>
+        );
+    }
 }
 
 export default Semester;
