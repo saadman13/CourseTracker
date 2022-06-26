453e19cd20a7d34d3949a7557c455c86278a5436
Saadman Islam Khan <saadmani@ualberta.ca>
2022-06-26 17:25:03 -0600

    add models for the database

--- src/backend/app/models.py
+++ src/backend/app/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+class Semester(models.Model):
+    name = models.CharField(max_length=255)
+    made_by = models.ForeignKey(CustomUser, related_name='semesters', on_delete=models.CASCADE)
+
+class Course(models.Model):
+    name = models.CharField(max_length=255)
+    belongs_to = models.ForeignKey(Semester, related_name='courses', on_delete=models.CASCADE)
+
+
