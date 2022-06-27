6b56ad7415ac0bd7c69da4ce43cb2208642bf26b
Saadman Islam Khan <saadmani@ualberta.ca>
2022-06-27 15:33:15 -0600

    add sign-in form

--- src/frontend/src/Components/Home.js
+++ src/frontend/src/Components/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Home.css';
 import { Link } from 'react-router-dom';
 import Signup from './Signup';
+import Signin from './Signin';
 
 
 export default class Home extends React.Component {
@@ -11,23 +12,30 @@ export default class Home extends React.Component {
         this.state = {
             isSignup: true
         }
-        this.toggleButton = this.toggleButton.bind(this);
+        this.signupToggleHandler = this.signupToggleHandler.bind(this);
+        this.signinToggleHandler = this.signinToggleHandler.bind(this);
     }
 
-    toggleButton(){
-        console.log("hello");
+    signupToggleHandler(){
         this.setState((state) => {
-            return {...state, isSignup: !state.isSignup}
+            return {...state, isSignup: true}
         })
     }
+
+    signinToggleHandler(){
+        this.setState((state) => {
+            return {...state, isSignup: false}
+        })
+    }
+
     render(){
         return (
         <div class='main-main-container'>
             <h1> Lets manage your courses!</h1>
             <div class='main-container'>
                 <div class="btn-container">
-                    <Link onClick={this.toggleButton} to='/signup' className='button sign-up'>Sign up</Link>
-                    <Link to='/signin' className='button sign-in'>Sign in</Link>
+                    <Link onClick={this.signupToggleHandler} to='/signup' className='button sign-up'>Sign up</Link>
+                    <Link onClick={this.signinToggleHandler} to='/signin' className='button sign-in'>Sign in</Link>
                 </div>
             </div>
             { this.state.isSignup && <Signup /> }
