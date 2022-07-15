07e42396a75aebd79b45d1ddcf12b47324dd2b37
Saadman Islam Khan <saadmani@ualberta.ca>
2022-07-14 22:43:23 -0600

    add style to the course details page

--- src/frontend/src/Components/CourseDetails.js
+++ src/frontend/src/Components/CourseDetails.js
@@ -11,6 +11,7 @@ import Paper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
+import './CourseDetails.css';
 
 function createData(name, calories, fat, carbs, protein) {
     return { name, calories, fat, carbs};
@@ -25,7 +26,7 @@ const rows = [
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
-      backgroundColor: theme.palette.warning.light,
+      backgroundColor: theme.palette.warning.dark,
       color: theme.palette.common.white,
     },
     [`&.${tableCellClasses.body}`]: {
@@ -51,9 +52,10 @@ const CourseDetails = () => {
     console.log(location.state.courseName);
     return (
         <div>
-            <h1>{location.state.courseName.toLocaleUpperCase()}</h1>
-            <button>Add a component</button>
-            <hr></hr>
+            <div className="main-title">
+                <h1 className="course-name-title">{location.state.courseName.toLocaleUpperCase()}</h1>
+                <button className="add-comp-btn">Add a component</button>
+            </div>
             <TableContainer component={Paper}>
                 <Table stickyHeader sx={{ minWidth: 650 }} aria-label="simple table">
                     <TableHead>
